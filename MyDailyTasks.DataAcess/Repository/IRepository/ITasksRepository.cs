895484d92aebf701849b75ca08ceb4bc2cf7b23f
Abderrahmane Chafi <abderrahmanedev@gmail.com>
2023-06-18 16:09:40 +0100

    update

---
+++ MyDailyTasks.DataAcess/Repository/IRepository/ITasksRepository.cs
@@ -0,0 +1,15 @@
+﻿using MyDailyTasks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDailyTasks.DataAcess.Repository.IRepository
+{
+    public interface ITasksRepository:IRepository<Tasks>
+    {
+        void Update(Tasks obj);
+
+    }
+}
---
+++ MyDailyTasks.DataAcess/Repository/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using MyDailyTasks.DataAcess.Data;
+using MyDailyTasks.DataAcess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDailyTasks.DataAcess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        
+        public ITasksRepository Tasks { get; private set; }
+
+        public IApplicationUserRepository ApplicationUser { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Tasks = new TasksRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
+
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
