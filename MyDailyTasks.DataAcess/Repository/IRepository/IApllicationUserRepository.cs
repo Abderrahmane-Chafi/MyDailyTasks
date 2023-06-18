4cc2cdf210e984f3ca9f5ba9c26836fa6ee70976
Abderrahmane Chafi <abderrahmanedev@gmail.com>
2023-06-18 18:41:50 +0100

    update

---
+++ MyDailyTasks.DataAcess/Repository/IRepository/IApllicationUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using MyDailyTasks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDailyTasks.DataAcess.Repository.IRepository
+{
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
+    {
+    }
+}
---
+++ MyDailyTasks.Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDailyTasks.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [DisplayName("First name")]        
+        public string FirstName { get; set; }
+        [DisplayName("Last name")]
+        public string LastName { get; set; }
+
+    }
+}
